9c1e36f7e3a8fa9940d7a48addb4d498b3db2c5c
Shreyojit <shreyojitdas95le.com>
2023-12-14 20:15:36 +0530

    Your commit message here

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import classes from './footer.module.css'
+
+const Footer = () => {
+  return (
+    <footer className={classes.footer}>
+      <div className={classes.wrapper}>
+        <div className={classes.col}>
+          <h2>About the App</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Voluptates velit fuga perspiciatis itaque iste, aliquid dignissimos voluptate modi,
+            tempore assumenda adipisci dolor hic atque quod consequuntur cupiditate. Quasi, nobis veritatis!
+          </p>
+        </div>
+        <div className={classes.col}>
+          <h2>Contacts</h2>
+          <span>Phone +123 456 789</span>
+          <span>YouTube: Life story</span>
+          <span>GitHub: sdas@12333</span>
+        </div>
+        <div className={classes.col}>
+          <h2>Location</h2>
+          <span>Continent: Asia</span>
+          <span>Country: India</span>
+          <span>Current Location: Bengal</span>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
